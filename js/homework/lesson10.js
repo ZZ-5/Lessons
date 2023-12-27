254042827e55755b91bdaea18090d8a08f784de5
alex173828 <alex173828@yandex.ru>
2023-12-27 16:15:55 +0300

    added lesson 10

---
+++ js/homework/lesson10.js
@@ -0,0 +1,3 @@
+// 1. https://learn.javascript.ru/closure - ещё раз почитать про замыкания и сделать задачи  в конце + коспект сюда же + задачи после главы
+// 2. https://learn.javascript.ru/garbage-collection - почитать про сборку мусора в js
+// 3.
