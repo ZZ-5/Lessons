f762ce3488acb9e01de2a9b47a527a3e1d6e7785
ZZ-5 <ivannikov.pavel.1994@gmail.com>
2024-01-11 19:15:55 +0300

    Gabella

--- js/homework/lesson20.js
+++ js/homework/lesson20.js
@@ -4,17 +4,34 @@
 // Каждый вызов должен сохраняться как массив аргументов.
 
 // Например:
-// function work(a, b) {
-//   console.log(a + b); // произвольная функция или метод
-// }
+function work(a, b) {
+    console.log(a + b); // произвольная функция или метод
+}
+
+function mul(x, y, z) {
+    console.log(x * y * z);
+}
+
+function spy(funk) {
+    return function wrap(...args) {
+        wrap.calls = []
+        wrap.calls.push(args)
+        return funk.apply(this, args);
+    }
+}
+
+let decorMul = spy(mul)
+
+decorMul(1, 2, 4)
+decorMul(4, 5, 6)
 
 // work = spy(work);
 
 // work(1, 2); // 3
 // work(4, 5); // 9
 
-// for (let args of work.calls) {
-//   console.log("call:" + args.join()); // "call:1,2", "call:4,5"
-// }
+for (let args of decorMul.calls) {
+    console.log("call:" + args.join()); // "call:1,2", "call:4,5"
+}
 
 // ваш код:
