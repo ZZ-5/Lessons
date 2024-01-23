86cfd8f8b7be8eed96cd798c133b58de80b956d2
ZZ-5 <ivannikov.pavel.1994@gmail.com>
2024-01-23 18:45:56 +0300

    lesson29/homework

--- js/homework/lesson29.js
+++ js/homework/lesson29.js
@@ -12,7 +12,40 @@ let obj = {
   Paul: {
     grade: "junior",
     salary: [0, 120000, 150000, 999999],
-  },
+  }
 };
 
-greatestSalary(obj);
+
+let greatestSalary = (obj) => {
+
+  let entries = Object.entries(obj)
+
+  let values = Object.values(obj)
+
+  values.forEach((el) => {
+
+    for (let key in el) {
+
+      if (Array.isArray(el[key])) {
+
+        let length = el[key].length;
+
+        el[key] = el[key].reduce((res, item) => {
+          res = res + item
+          return res
+        }, 0)
+
+        el[key] = el[key] / length
+        Math.round(el[key])
+
+        return el[key] // Math.round почему-то сбрасывается
+      }
+    }
+  })
+
+  entries.sort((a, b) => b[1].salary - a[1].salary)
+
+  return `${entries[0][0]} is a senior developer and his salary is ${entries[0][1].salary}`;
+}
+
+console.log(greatestSalary(obj));
